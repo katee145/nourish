ea8e2d44e11a9f5e75633b32639a949150b7b1eb
Kate Elliott <kateelliott145@gmail.com>
2025-01-14 15:32:12 +0000

    add placeholder post list html

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nourish_home.urls')),
+    path('', include('nourish_home.urls') name="home-urls"),
 ]
