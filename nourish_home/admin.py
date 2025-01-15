b29c39bd8b7a47148519e0a53ac10b47f029c485
Kate Elliott <kateelliott145@gmail.com>
2025-01-15 14:41:36 +0000

    install and configure summernote

--- nourish_home/admin.py
+++ nourish_home/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
 admin.site.register(Recipe)
