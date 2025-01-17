34078ec3a1f3e6ca810dd592241cde9a903ed830
Kate Elliott <kateelliott145@gmail.com>
2025-01-17 17:18:14 +0000

    add delete button with confirmation

--- nourish_home/urls.py
+++ nourish_home/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('<slug:slug>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 
 ]
--- nourish_home/views.py
+++ nourish_home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.views import View
 from django.views import generic
 from django.contrib import messages
@@ -66,24 +67,43 @@ class RecipeDetailView(View):
         })
 
 def comment_edit(request, slug, comment_id):
-    #view to edit comments
-        if request.method == "POST":
+    """
+    View to edit comments
+    """
+    # Step 1: Corrected recipe retrieval before usage
+    recipe = get_object_or_404(Recipe, slug=slug, status=1)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    comment_form = CommentForm(data=request.POST, instance=comment)
 
-            queryset = recipe.objects.filter(status=1)
-            recipe = get_object_or_404(queryset, slug=slug)
-            comment = get_object_or_404(Comment, pk=comment_id)
-            comment_form = CommentForm(data=request.POST, instance=comment)
+    if request.method == "POST":
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            #comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
-            if comment_form.is_valid() and comment.author == request.user:
-                comment = comment_form.save(commit=False)
-                comment.recipe = recipe
-                comment.approved = False
-                comment.save()
-                messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-            else:
-                messages.add_message(request, messages.ERROR, 'Error updating comment!')
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
 
-        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+def comment_delete(request, slug, comment_id):
+    """
+    View to delete a comment
+    """
+    recipe = get_object_or_404(Recipe, slug=slug, status=1)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
+
+
 
 #    def comment_edit(request, slug, comment_id):
 #     """
